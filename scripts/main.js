626f7c2e4dc512e49706acbea2d971c9b4dea0cc
William Briscoe <will.m.briscoe@gmail.com>
2023-05-17 10:42:34 -0500

    createServiceHTML made and added missing div tag in guests module

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
 import { createGuestsHTML } from "./guests.js";
+import { createServicesHTML } from "./services.js";
 
 let guestsHTML = createGuestsHTML()
+let servicesHTML = createServicesHTML()
 
 let parentHTMLElement = document.querySelector('#container')
-parentHTMLElement.innerHTML = guestsHTML
+parentHTMLElement.innerHTML = guestsHTML + servicesHTML
+//make note of new closing div tag
